72cbd0a602d911288b52ee80f63f7abf152b794a
Ghulam Ahmed <noorghulam7153@gmail.com>
2025-08-23 04:32:17 -0400

    replaced all images with png backgrounds and changed cards to darker theme

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,27 +12,20 @@ const Card = ({
   pdfLinkLabel,
 }) => (
   <div className="mb-4">
-    <div
-      className="card mb-4 shadow-sm h-100 border-0"
-      style={{ maxWidth: "100%", boxShadow: "0 4px 12px rgba(0, 0, 0, 0.5)" }}
-    >
+    <div className="card mb-4 shadow-sm h-100 border-0">
       <div className="row g-0 h-100">
         <div className="col-md-4">
           <div className="card-img-wrapper">
-            <img
-              src={image}
-              alt={title}
-              className="card-img-custom rounded-start"
-            />
+            <img src={image} alt={title} className="card-img-custom" />
           </div>
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">
+            <h5 className="card-title text-light">{title}</h5>
+            <h6 className="card-subtitle mb-2">
               {company} • {date}
             </h6>
-            <p className="card-text">
+            <p className="card-text text-light">
               {description.split("\n").map((line, index) => (
                 <span key={index}>
                   {line}
